d6c43bd7bbf089fb1b84ac42e330882c165b7728
Manian VSS <manianvss@hotmail.com>
2026-03-19 01:36:44 +0530

    Add package.sh

--- package.bat
+++ package.bat
@@ -1,1 +1,5 @@
+# Create a zip file of the mod for distribution
 tar.exe -a -c -f MMZAccelerationMod_2.0.0.zip MMZAccelerationMod
+
+# Copy the zip file to the Factorio mods directory
+copy MMZAccelerationMod_2.0.0.zip "%APPDATA%\Factorio\mods\MMZAccelerationMod_2.0.0.zip"
---
+++ package.sh
@@ -0,0 +1,11 @@
+# Delete existing zip file if it exists
+if [ -f MMZAccelerationMod_2.0.0.zip ]; then
+    rm MMZAccelerationMod_2.0.0.zip
+fi
+
+# Package the mod folder MMZAccelerationMod into a zip file
+zip -r MMZAccelerationMod_2.0.0.zip MMZAccelerationMod
+
+#Copy factorio mod to factorio mod folder in linux
+mkdir -p ~/.factorio/mods
+cp MMZAccelerationMod_2.0.0.zip ~/.factorio/mods/
